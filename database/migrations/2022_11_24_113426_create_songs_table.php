da151dbc2d78eab72caec501da90a266d075fcba
Rory McManus <rorymc9@gmail.com>
2022-12-06 17:40:39 +0000

    song

--- database/migrations/2022_11_24_113426_create_songs_table.php
+++ database/migrations/2022_11_24_113426_create_songs_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('songs', function (Blueprint $table) {
             $table->id();
+            $table->name();
+            
             $table->timestamps();
         });
     }
